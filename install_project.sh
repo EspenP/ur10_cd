639898ee219299adfd180035ad25d87a47899c03
EspenP <espenp92@gmail.com>
2020-11-11 11:39:16 -0800

    Added files to add a camera model and change the position of the robot.

--- install_project.sh
+++ install_project.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# If this fails, just manually run these commands
 
 world_path="/usr/share/gazebo-7/worlds/"
 launch_path="~/catkin_ws/src/ur_gazebo/launch/"
 model_path="~/.gazebo/models/"
+urdf_path="~/catkin_ws/src/ur_description/urdf/"
+upload_path="~/catkin_ws/src/ur_description/launch/"
 
-cp -r launch/* launch_path && echo "Copied launch files"
-(cp -r worlds/* world_path && echo "Copied world files") || echo "PLEASE RUN THIS AS ROOT"
-mkdir -p model_path
-cp -r models/* model_path && echo "Copied model files"
+cp gzlaunch/* launch_path && echo "Copied launch files"
+cp upload_launch/* upload_path && echo "Copied upload launch files"
+(cp worlds/* world_path && echo "Copied world files") || echo "PLEASE RUN THIS AS ROOT"
+cp -r models/* ~/.gazebo/models/ && echo "Copied model files"
+cp urdf/* ~/catkin_ws/src/ur_description/launch/ && echo "Copied URDF files" 
 echo "DONE"
